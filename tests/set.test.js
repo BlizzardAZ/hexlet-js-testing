29b408b7feba757b2a00b1e6d736956fca87056f
Alina Zauer <sauer6699@gmail.com>
2025-07-21 23:53:23 +0500

    refactor tests code, add package.json

--- tests/set.test.js
+++ tests/set.test.js
@@ -1,15 +1,18 @@
+import { strict as assert } from 'node:assert'
 import set from '../src/set.js'
 
 const obj = {}
 
 set(obj, 'key1', 'value1')
-if (obj.key1 !== 'value1') {
-  throw new Error('Функция работает неверно!')
-}
+assert.deepEqual(obj.key1, 'value1', 'Функция работает неверно!')
+// if (obj.key1 !== 'value1') {
+//   throw new Error('Функция работает неверно!')
+// }
 
 set(obj, 'key1', 'next value')
-if (obj.key1 !== 'next value') {
-  throw new Error('Функция работает неверно!')
-}
+assert.deepEqual(obj.key1, 'next value', 'Функция работает неверно!')
+// if (obj.key1 !== 'next value') {
+//   throw new Error('Функция работает неверно!')
+// }
 
 console.log('Все тесты пройдены!')
